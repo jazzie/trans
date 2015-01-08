c83b19cf49bf203796518936ec051915b35a5427
jazzie <jessicaginachen@jazziedeMacBook-Air.local>
2015-01-08 21:14:36 +0800

    minor change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function myCtrl($scope){
         	title:'國際跨性別紀念日 Transgender Day of Remembrance',
         	text:'國際跨性別紀念日(英文：Transgender Day of Remembrance)日期為11月20日。它的設立是為了紀念因反跨性別的憎恨或偏見而遭殺害的跨性別朋友。這個行動的舉行是為了向1998年11月28日被謀殺的Rita Hester致敬所發起的，始於1998年的〝Remembering Our Dead〞（紀念我們的死者）網站計畫以及1999年在舊金山舉行的燭光守夜活動。Rita Hester的死如同大多數反跨性別的謀殺案件，至今尚未有結果。',}
          
-         {<link>}
+         
 
          ]
     
